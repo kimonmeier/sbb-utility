eb07a421c7bec11e53f241cf745ef5245443dd9b
Kimon Meier <kimon.meier@gmail.com>
2026-05-01 13:37:05 +0200

    Added blank "Bremsrechner" page

---
+++ src/routes/bremsrechner/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages';
+</script>
+
+<svelte:head>
+	<title>{m.bremsrechner_title()}</title>
+	<meta name="description" content={m.bremsrechner_meta_description()} />
+</svelte:head>
